cf7df024f7a28f8aba8fe9dfae58094eafb6122b
nargok <nargok@gmail.com>
2018-12-22 15:48:06 +0900

    implement Link Query to server

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import Link from './Link';
 
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
+
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma replaces traditional ORMs and makes it easy to build GraphQL servers 😎',
-        url: 'https://www.prisma.io',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
     return (
-      <div>
-        {linksToRender.map(link => <Link key={link.id} link={link} />)}
-      </div>
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching...</div>
+          if (error) return <div>Error</div>
+
+          const linksToRender = data.feed.links
+
+          return (
+            <div>
+              {() => linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )
+        }}
+      </Query>
     )
   }
 };
