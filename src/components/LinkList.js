cf5f659fbdfbba04132b8a093f699280411a8503
nargok <nargok@gmail.com>
2018-12-23 18:28:50 +0900

    subscribe new link creation

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -4,6 +4,27 @@ import Link from './Link';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
 export const FEED_QUERY = gql`
   {
     feed {
@@ -35,12 +56,32 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,  // sucscription will fire every time a New Link created
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Fetching...</div>
           if (error) return <div>Error</div>
+
+          this._subscribeToNewLinks(subscribeToMore);
 
           const linksToRender = data.feed.links
 
