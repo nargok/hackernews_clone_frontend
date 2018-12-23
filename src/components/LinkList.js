9f2b9bf11837acbb05b913f8e7ee474df24d722c
nargok <nargok@gmail.com>
2018-12-23 12:06:35 +0900

    implement Vote mutation with update votes count

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -28,6 +28,13 @@ const FEED_QUERY = gql`
 `
 
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY }); // キャッシュからQuryでデータを取り出す
+    const votedLink = data.feed.links.find(link => link.id === linkId); // キャッシュデータからvoteしたリンクのデータを取得する
+    votedLink.votes = createVote.link.votes;  // Mutationの戻り値で取得したvotesデータで、キャッシュのvotesデータを上書きする
+    store.writeQuery({ query: FEED_QUERY, data });
+  };
+
   render() {
     return (
       <Query query={FEED_QUERY}>
@@ -40,7 +47,12 @@ class LinkList extends Component {
           return (
             <div>
           {linksToRender.map((link, index) => (
-            <Link key={link.id} link={link} index={index} />
+            <Link
+              key={link.id}
+              link={link}
+              index={index}
+              updateStoreAfterVote={this._updateCacheAfterVote}
+            />
           ))}
             </div>
           )
